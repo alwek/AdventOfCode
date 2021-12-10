9550de638c195868442850134480a424d51618ef
Alican Bircan <alican.bircan@knowit.se>
2021-12-10 10:58:58 +0100

    added solution for day 10

--- AdventOfCode2021/Day10.cs
+++ AdventOfCode2021/Day10.cs
@@ -1,24 +1,66 @@
-namespace AdventOfCode2021
-{
-    internal static class Day10
-    {
-        public static void Run(string path)
-        {
+namespace AdventOfCode2021 {
+    internal static class Day10 {
+        public static void Run(string path) {
             Console.WriteLine("Day 10");
-            var input = File.ReadAllText(path);
+            List<string> input = FileHelper.ReadFileAsList(path);
 
-            PartOne();
-            PartTwo();
+            Solve(input);
         }
 
-        private static void PartOne()
-        {
+        private static void Solve(List<string> input) {
+            int corruptPoints = 0;
+            List<long> incompletePoints = new();
+            Dictionary<char, char> bracketPairs = new() {
+                { '(', ')' },
+                { '[', ']' },
+                { '{', '}' },
+                { '<', '>' },
+            };
+
+            foreach(string line in input) {
+                Stack<char> brackets = new();
+                bool incomplete = true;
+
+                foreach (char character in line) {
+                    if(bracketPairs.ContainsKey(character))
+                        brackets.Push(character);
+                    else {
+                        char openingBracket = brackets.Pop();
+                        if (!bracketPairs[openingBracket].Equals(character)) {
+                            Console.WriteLine($"Expected {openingBracket}, but found {character} instead");
+                            corruptPoints += CalculateCorruptPoints(character);
+                            incomplete = false;
+                        } 
+                    }
+                }
+
+                if (incomplete) 
+                    incompletePoints.Add(CalculateIncompletePoints(brackets));
+            }
+
+            incompletePoints.Sort();
             Console.WriteLine("Part One");
+            Console.WriteLine($"Sum of corrupt lines is {corruptPoints}");
+            Console.WriteLine("Part Two");
+            Console.WriteLine($"Middle score of incomplete lines is {incompletePoints[incompletePoints.Count / 2]}");
         }
 
-        private static void PartTwo()
-        {
-            Console.WriteLine("Part Two");
+        private static int CalculateCorruptPoints(char character) => 
+            character.Equals(')') ? 3 : 
+            character.Equals(']') ? 57 : 
+            character.Equals('}') ? 1197 : 
+            25137;
+
+        private static long CalculateIncompletePoints(Stack<char> brackets){
+            long points = 0;
+            while(brackets.Count > 0) {
+                char character = brackets.Pop();
+                points = points * 5 + (character.Equals('(') ? 1 :
+                    character.Equals('[') ? 2 :
+                    character.Equals('{') ? 3 : 4);
+            }
+
+            return points;
         }
     }
 }
