9ae2f95f63dd2b75292bae8ffa56958be57a264c
Alican Bircan <alican.bircan@knowit.se>
2021-12-21 14:56:57 +0100

    added solution for day 21 part 2

--- AdventOfCode2021/Day21.cs
+++ AdventOfCode2021/Day21.cs
@@ -1,17 +1,21 @@
 ﻿namespace AdventOfCode2021 {
     internal class Day21 {
+        private static List<long>[] PlayerWins = new List<long>[2] {new(), new()};
+
         public static void Run(string path) {
             Console.WriteLine("Day 21");
             var input = FileHelper.ReadFileAsList(path);
 
             Console.WriteLine("Part One");
-            Solve(input);
+            PartOne(input);
+            Console.WriteLine("Part Two");
+            PartTwo(input);
         }
 
-        private static void Solve(List<string> input) {
+        private static void PartOne(List<string> input) {
             (int position, int score)[] players = { 
-                (int.Parse(input[0][^2] + input[0][^1].ToString()), 0), 
-                (int.Parse(input[1][^2] + input[1][^1].ToString()), 0) };
+                (int.Parse(input[0][^2..]), 0), 
+                (int.Parse(input[1][^2..]), 0) };
             int dice = 0;
 
             while (players[0].score < 1000 && players[1].score < 1000) {
@@ -24,6 +28,46 @@
             }
 
             Console.WriteLine($"The product is {players.First(player => player.score < 1000).score * dice}");
+        }
+
+        private static void PartTwo(List<string> input) {
+            (long position, long score)[] players = {
+                (int.Parse(input[0][^2..]), 0),
+                (int.Parse(input[1][^2..]), 0) };
+            var (p1, p2) = QuantumDice(players, 0);
+            Console.WriteLine($"Player one wins {p1} times and player two wins {p2} times");
+        }
+
+        private static (long p1, long p2) QuantumDice((long position, long score)[] players, int turn) {
+            if (players[0].score >= 21)
+                return (1, 0);
+            else if (players[1].score >= 21)
+                return (0, 1);
+
+            var newPlayers1 = new (long position, long score)[2] { players[0], players[1] };
+            newPlayers1[turn].position = (newPlayers1[turn].position + 1) % 10 == 0 
+                ? newPlayers1[turn].position 
+                : (newPlayers1[turn].position + 1) % 10;
+            newPlayers1[turn].score += newPlayers1[turn].position;
+
+            var newPlayers2 = new (long position, long score)[2] { players[0], players[1] };
+            newPlayers2[turn].position = (newPlayers2[turn].position + 2) % 10 == 0
+                ? newPlayers2[turn].position
+                : (newPlayers2[turn].position + 2) % 10;
+            newPlayers2[turn].score += newPlayers2[turn].position;
+
+            var newPlayers3 = new (long position, long score)[2] { players[0], players[1] };
+            newPlayers3[turn].position = (newPlayers3[turn].position + 3) % 10 == 0
+                ? newPlayers3[turn].position
+                : (newPlayers3[turn].position + 3) % 10;
+            newPlayers3[turn].score += newPlayers3[turn].position;
+
+            turn++;
+            var result1 = QuantumDice(newPlayers1, turn % 2);
+            var result2 = QuantumDice(newPlayers2, turn % 2);
+            var result3 = QuantumDice(newPlayers3, turn % 2);
+
+            return (result1.p1 + result2.p1 + result3.p1, result1.p2 + result2.p2 + result3.p2);
         }
     }
 }
