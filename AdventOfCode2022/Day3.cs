73895a323b6c353223da248ae8765115796cb608
Alican Bircan <alican.bircan@knowit.se>
2022-12-03 07:37:22 +0100

    improved day 3 solution

--- AdventOfCode2022/Day3.cs
+++ AdventOfCode2022/Day3.cs
@@ -13,11 +13,12 @@
 
             foreach (string item in input) {
                 int half = item.Length / 2;
-                char[] splitted = item.ToCharArray();
-                char[] first = splitted.Take(half).ToArray();
-                char[] second = splitted.TakeLast(half).ToArray();
+                var first = item.Take(half);
+                var second = item.TakeLast(half);
 
-                char letter = first.Where(x => second.Contains(x)).FirstOrDefault();
+                char letter = first
+                    .Where(x => second.Contains(x))
+                    .FirstOrDefault();
                 count += Char.IsLower(letter) ? letter - 96 : letter - 38;
             }
 
@@ -27,12 +28,14 @@
         private static void PartTwo(List<string> input) {
             int count = 0;
 
-            for (int i = 0; i < input.Count - 2; i += 3) { 
-                var first = input[i].ToCharArray();
-                var second = input[i + 1].ToCharArray();
-                var third = input[i + 2].ToCharArray();
+            for (int i = 0; i < input.Count - 2; i += 3) {
+                var first = input[i];
+                var second = input[i + 1];
+                var third = input[i + 2];
 
-                char letter = first.Where(x => second.Contains(x) && third.Contains(x)).FirstOrDefault();
+                char letter = first
+                    .Where(x => second.Contains(x) && third.Contains(x))
+                    .FirstOrDefault();
                 count += Char.IsLower(letter) ? letter - 96 : letter - 38;
             }
 
