bae4fe648a8c0a7fec2229a6f77a7767eb49bcc8
Alican Bircan <alican.bircan@knowit.se>
2022-12-05 08:40:34 +0100

    Improved solution for day 5

--- AdventOfCode2022/Day5.cs
+++ AdventOfCode2022/Day5.cs
@@ -1,5 +1,4 @@
-﻿using System.Runtime.CompilerServices;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 namespace AdventOfCode2022 {
     internal static class Day5 {
@@ -7,11 +6,11 @@ namespace AdventOfCode2022 {
             Console.WriteLine("Day Five");
             List<string> input = FileHelper.ReadFileAsList(path);
 
-            PartOne(input);
-            PartTwo(input);
+            Solve(input, false);
+            Solve(input, true);
         }
 
-        private static void PartOne(List<string> input) {
+        private static void Solve(List<string> input, bool isPartTwo) {
             var index = input.IndexOf(string.Empty);
             var setup = input.Take(index - 1).Reverse();
             var instructions = input.Take(new Range(index + 1, input.Count));
@@ -22,9 +21,9 @@ namespace AdventOfCode2022 {
             for(int i = 0; i < max; i++)
                 stacks.Add(new Stack<string>(100));
 
-            foreach (var line in setup) {
+            foreach (var item in setup) {
                 foreach (Match position in positions.Cast<Match>()) {
-                    string character = line[position.Index].ToString();
+                    string character = item[position.Index].ToString();
                     int value = int.Parse(position.Value);
 
                     if(!string.IsNullOrWhiteSpace(character))
@@ -38,62 +37,33 @@ namespace AdventOfCode2022 {
                 int from = int.Parse(match[1].Value);
                 int to = int.Parse(match[2].Value);
 
-                for (int i = 0; i < amount; i++) {
-                    string box = stacks[from - 1].Pop();
-                    stacks[to - 1].Push(box);
-                }
+                if(isPartTwo)
+                    PartTwo(stacks, amount, from, to);
+                else
+                    PartOne(stacks, amount, from, to);
             }
 
             var result = string.Concat(stacks.Select(x => x.Peek()));
             Console.WriteLine(result);
         }
 
-        private static void PartTwo(List<string> input) {
-            var index = input.IndexOf(string.Empty);
-            var numbers = input[index - 1];
-            var setup = input.Take(index - 1).Reverse();
-            var instructions = input.Take(new Range(index + 1, input.Count));
-            var positions = Regex.Matches(numbers, "\\d+");
-            var stacks = new List<Stack<string>>();
-
-            int max = int.Parse(positions.Last().Value);
-            for (int i = 0; i < max; i++) {
-                stacks.Add(new Stack<string>(100));
+        private static void PartTwo(List<Stack<string>> stacks, int amount, int from, int to) {
+            var temporary = new Stack<string>();
+            for (int i = 0; i < amount; i++) {
+                string box = stacks[from - 1].Pop();
+                temporary.Push(box);
             }
-
-            foreach (var line in setup) {
-                foreach (Match position in positions) {
-                    string character = line[position.Index].ToString();
-                    int value = int.Parse(position.Value);
-
-                    if (string.IsNullOrWhiteSpace(character))
-                        continue;
-                    else
-                        stacks[value - 1].Push(character);
-                }
+            for (int i = 0; i < amount; i++) {
+                string box = temporary.Pop();
+                stacks[to - 1].Push(box);
             }
+        }
 
-            foreach (var instruction in instructions) {
-                var match = Regex.Matches(instruction, "\\d+");
-
-                int amount = int.Parse(match[0].Value);
-                int from = int.Parse(match[1].Value);
-                int to = int.Parse(match[2].Value);
-                var queue = new Stack<string>();
-
-                for (int i = 0; i < amount; i++) {
-                    string box = stacks[from - 1].Pop();
-                    queue.Push(box);
-                }
-
-                for (int i = 0; i < amount; i++) {
-                    string box = queue.Pop();
-                    stacks[to - 1].Push(box);
-                }
+        private static void PartOne(List<Stack<string>> stacks, int amount, int from, int to) {
+            for (int i = 0; i < amount; i++) {
+                string box = stacks[from - 1].Pop();
+                stacks[to - 1].Push(box);
             }
-
-            var result = string.Concat(stacks.Select(x => x.Peek()));
-            Console.WriteLine(result);
         }
     }
 }
