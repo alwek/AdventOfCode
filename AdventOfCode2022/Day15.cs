95d000d3299ad334c5574affe830a5edca2c7b6d
Alican Bircan <alican.bircan@knowit.se>
2023-02-22 21:52:21 +0100

    Added partial solution for day 15 part 2

--- AdventOfCode2022/Day15.cs
+++ AdventOfCode2022/Day15.cs
@@ -7,12 +7,15 @@
             Solve(input);
         }
 
+        private const int Max = 4000000;
         private static readonly int Row = 2000000;
 
         private static void Solve(List<string> input) {
             string[] seperator = new string[] { " ", ",", "=", ":" };
             Dictionary<((long, long), (long, long)), long> beaconsensors = new();
             List<(long x, long y)> points = new();
+            List<(long x, long y)> perimeter = new();
+            List<(long x, long y)> available = new();
 
             foreach (string line in input) {
                 string[] splitted = line.Split(seperator, StringSplitOptions.RemoveEmptyEntries);
@@ -29,10 +32,22 @@
                 points.Add(bs.Key.Item1);
                 points.AddRange(PointsInRow(bs.Key.Item1, bs.Value));
                 points.Remove(bs.Key.Item2);
+
+                perimeter.AddRange(PointsInRadius(bs.Key.Item1, bs.Value));
+            }
+
+            foreach(var bs in beaconsensors) {
+                (long x, long y) sensor = bs.Key.Item1;
+                var c1 = (sensor.x - bs.Value, sensor.y);
+                var c2 = (sensor.x + bs.Value, sensor.y);
+                var c3 = (sensor.x, sensor.y - bs.Value);
+                var c4 = (sensor.x, sensor.y + bs.Value);
+
+                available.AddRange(perimeter.Where(point => !IsPositionInsideRectangle(c1, c2, c3, c4, point) && IsWithinBoundries(point)));
             }
 
             var positions = points.Where(point => point.y == Row).Distinct().Count();
-            Console.WriteLine(positions);
+             Console.WriteLine(positions);
         }
 
         private static IEnumerable<(long x, long y)> PointsInRow((long x, long y) point, long radius) {
@@ -47,21 +62,41 @@
         }
 
         private static IEnumerable<(long x, long y)> PointsInRadius((long x, long y) point, long radius) {
-            bool peak = false;
-            long i = 0;
+            (long j1, long j2, long j3, long j4) = (point.y, point.y + radius + 1, point.y, point.y - radius - 1);
 
-            for (long x = point.x - radius; x < point.x + radius; x++) {
-                for (long y = point.y - i; y < point.y + i; y++)
-                    yield return (x, y);
-
-                if (i == radius)
-                    peak = true;
-
-                if (peak)
-                    i--;
-                else
-                    i++;
+            for (long i = point.x - radius - 1; i <= point.x + radius + 1; i++) {
+                if (i < point.x) {
+                    yield return (i, j1++);
+                    yield return (i, j3--);
+                }
+                else if (i > point.x) {
+                    yield return (i, j2--);
+                    yield return (i, j4++);
+                }
+                else {
+                    yield return (i, j1++);
+                    yield return (i, j3--);
+                    yield return (i, j2--);
+                    yield return (i, j4++);
+                }
             }
         }
+
+        private static bool IsPositionInsideRectangle(
+            (long x, long y) c1, 
+            (long x, long y) c2, 
+            (long x, long y) c3, 
+            (long x, long y) c4, 
+            (long x, long y) c5) {
+
+            if (c5.x >= Math.Min(c1.x, c4.x) && c5.x <= Math.Max(c2.x, c3.x)) 
+                if (c5.y >= Math.Min(c1.y, c2.y) && c5.y <= Math.Max(c3.y, c4.y)) 
+                    return true;
+
+            return false;
+        }
+
+        private static bool IsWithinBoundries((long x, long y) point) => 
+            point.x >= 0 && point.x <= Max && point.y >= 0 && point.y <= 20;
     }
 }
